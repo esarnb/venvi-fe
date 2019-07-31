a548efcc91c33d6fe8bcc2529a8e9e7d213b14e4
Phillip Chang <phillip-chang@live.com>
2019-07-31 16:38:05 -0700

    Updates

---
+++ precache-manifest.b3d6522a2dd22ca9b09fd07b6174f5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050a5d87a5d61841fbf4031d885b6772",
+    "url": "/venvi-fe/index.html"
+  },
+  {
+    "revision": "ed38913c9f96ae475e95",
+    "url": "/venvi-fe/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/venvi-fe/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ed38913c9f96ae475e95",
+    "url": "/venvi-fe/static/js/main.e3e916da.chunk.js"
+  },
+  {
+    "revision": "7b77bfe0db83383134dc",
+    "url": "/venvi-fe/static/js/runtime~main.6dba387e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/venvi-fe/static/media/logo.5d5d9eef.svg"
+  }
+]);
