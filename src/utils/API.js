fcf967f6a091cb453879712af7014db309a64043
Phillip Chang <phillip-chang@live.com>
2019-07-31 23:30:38 -0700

    Add routes and base url

---
+++ src/utils/API.js
@@ -0,0 +1,24 @@
+import app from "/.index";
+
+export default {
+
+    getResponse() {
+      app.get("/api/vehicle/all").then(res => console.log(res.data));
+    },
+  
+    postQuery() {
+      app.post("/api/vehicle/new", {msg: "Vehicle has been added"}).then(res => console.log(res.data));
+    },
+  
+    putQuery(id) {
+      app.put(`/api/vehicle/update/${id}`, {msg: "update this listing"}).then(res => {
+        console.log(res.data);
+      })
+    },
+  
+    deleteQuery(id) {
+      app.delete(`/api/vehicle/delete/${id}`, {msg: "delete this listing"}).then(res => {
+        console.log(res.data);
+      })
+    }
+  }
