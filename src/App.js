137565c1759088aedb5cfbd83cf9ef00a9adc27a
Phillip Chang <phillip-chang@live.com>
2019-08-01 11:19:29 -0700

    Add query functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import API from '../src/Utils/API'
 
-function App() {
+class App extends React.Component {
+
+  componentDidMount() {
+  let id = 123;
+  API.postQuery();
+  API.getResponse();
+  API.putQuery(id);
+  API.deleteQuery(id);
+}
+
+render () {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +32,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
