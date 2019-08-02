d74b884699f5392e61c1297c9d7055bcd7771b09
Chloe Zhou <chloezhouny@gmail.com>
2019-08-02 14:44:09 -0700

    Add border and change top consumer pick to h1 tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import Banner from '../components/Banner';
 import ButtonBases from '../components/ButtonBases';
-import Container from '../components/Container'
+import Container from '../components/Container';
+import './index.css'
 
 
 class Home extends React.Component {
@@ -13,6 +14,8 @@ render () {
         <NavBar/>
         <Banner />
         <Container>
+        	<h1> Top Consumer Picks </h1>
+        	<span id="line"> </span>
         	<ButtonBases />
         </Container>
     </div>
