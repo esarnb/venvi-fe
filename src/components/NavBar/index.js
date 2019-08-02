030e157afdba8cae03ff26230672a728d37e45c7
Phillip Chang <phillip-chang@live.com>
2019-08-01 23:55:37 -0700

    Add NavBar and styling

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import './style.css';
+
+const AntTabs = withStyles({
+  root: {
+    borderBottom: "1px solid black"
+  },
+  indicator: {
+    backgroundColor: "#red"
+  }
+})(Tabs);
+
+const AntTab = withStyles(theme => ({
+  root: {
+    textTransform: "none",
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(4),
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:hover": {
+      color: "green",
+      opacity: 1
+    },
+    "&$selected": {
+      color: "white",
+      fontWeight: theme.typography.fontWeightMedium
+    },
+    "&:focus": {
+      color: "green"
+    }
+  },
+  selected: {}
+}))(props => <Tab disableRipple {...props} />);
+
+const StyledTabs = withStyles({
+  indicator: {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    "& > div": {
+      maxWidth: 40,
+      width: "100%",
+      backgroundColor: "gold"
+    }
+  }
+})(props => <Tabs {...props} TabIndicatorProps={{ children: <div /> }} />);
+
+const StyledTab = withStyles(theme => ({
+  root: {
+    textTransform: "none",
+    color: "#fff",
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    "&:focus": {
+      opacity: 1
+    }
+  }
+}))(props => <Tab disableRipple {...props} />);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  padding: {
+    padding: theme.spacing(3)
+  },
+  demo1: {
+    backgroundColor: theme.palette.background.paper
+  },
+  demo2: {
+    backgroundColor: "black"
+  }
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.demo1}>
+        <Typography className={classes.padding} />
+      </div>
+      <div className={classes.demo2}>
+        <StyledTabs
+          value={value}
+          onChange={handleChange}
+          aria-label="styled tabs example"
+        >
+          <div>Hi</div>
+          <StyledTab label="Chloe" />
+          <StyledTab label="Supreme" />
+          <StyledTab label="Leader" />
+        </StyledTabs>
+        <Typography className={classes.padding} />
+      </div>
+    </div>
+  );
+}
+
+
+export default Navbar;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+
+class Home extends React.Component {
+
+render () {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  );
+}
+}
+
+export default Home;
