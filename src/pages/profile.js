d90ca40114dc1cb8a811fa83fd149fb5d121746a
Phillip Chang <phillip-chang@live.com>
2019-08-03 15:48:18 -0700

    Add Avatar and listing card

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -4,16 +4,30 @@ import Banner from '../components/Banner';
 import ButtonBases from '../components/ButtonBases';
 import Container from '../components/Container';
 import UserProfile from '../components/UserProfile';
+import PostCard02 from '../components/ListCard';
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import createOverrides from '../components/ListCard/';
+
+
+const baseTheme = createMuiTheme();
 
 
 class Profile extends React.Component {
-
-render () {
+render () {    
   return (
     <div>
         <NavBar/>
         <Container>
         <UserProfile/>
+        <ThemeProvider
+        theme={{
+        ...baseTheme,
+        overrides: createOverrides(baseTheme)
+        }}
+        >
+        <PostCard02/>
+        </ThemeProvider>
         </Container>
     </div>
   );
