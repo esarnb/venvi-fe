ca399870e346efcf553a5e93d2dc75474661c0d9
Phillip Chang <phillip-chang@live.com>
2019-08-03 00:01:13 -0700

    Add vehicle routes

--- src/utils/VehicleAPI.js
+++ src/utils/VehicleAPI.js
@@ -7,22 +7,11 @@ export default {
   // },
 
   // Reviews get all reviews of cars
-  getReviews() {
+  getVehicles() {
     app.get("/api/review").then(res => console.log(res.data));
   },
     postQuery() {
       app.post("/api/vehicle/new", {msg: "Vehicle has been added"}).then(res => console.log(res.data));
     },
-  
-    putQuery(id) {
-      app.put(`/api/vehicle/update/${id}`, {msg: "update this listing"}).then(res => {
-        console.log(res.data);
-      })
-    },
-  
-    deleteQuery(id) {
-      app.delete(`/api/vehicle/delete/${id}`, {msg: "delete this listing"}).then(res => {
-        console.log(res.data);
-      })
-    }
+
   }
