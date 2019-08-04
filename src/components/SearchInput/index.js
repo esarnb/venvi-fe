8b9b3a192c3cbc3a39135ba0b72996a7b1feac8a
b0bland <alfredychan96@gmail.com>
2019-08-04 15:20:28 -0700

    Add model check

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -19,16 +19,36 @@ searchAction() {
     axios.get("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json")
         .then(response => {
             var allMakes = response.data.Results;
-            var queryMake = this.state.make;
+            var queryMake = this.state.make.toLowerCase();
 
             console.log(queryMake);
 
-            allMakes.map((value) => {
-                if(value.MakeName.indexOf(queryMake) > -1) {
+            allMakes.map(value => {
+                if(value.MakeName.toLowerCase().indexOf(queryMake) > -1) {
                     console.log("Make match!")
                     // makeVeri = true;
                 }
             })
+
+            axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${this.state.make}/vehicleType/car?format=json`)
+                .then(response => {
+                    var carModels = response.data.Results;
+                    axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${this.state.make}/vehicleType/truck?format=json`)
+                        .then(resp => {
+                            var truckModels = resp.data.Results;
+                            var allModels = carModels.concat(truckModels);
+                            console.log(allModels);
+                            var queryModel = this.state.model.toLowerCase();
+                            console.log(queryModel);
+
+                            allModels.map(value => {
+                                if(value.Model_Name.toLowerCase().indexOf(queryModel) > -1) {
+                                    console.log("Model match!")
+                                    // modelVeri = true;
+                                }
+                            })
+                        })
+                })
 
         })
 }
@@ -51,7 +71,7 @@ render () {
                     <TextInput name="make" value={this.state.make} onChange={this.handleChange} type="text" label="Make" />                    
                 </Col>
                 <Col className="input-field">
-                    <TextInput name="model" type="text" label="Model" />                    
+                    <TextInput name="model" value={this.state.model} onChange={this.handleChange} type="text" label="Model" />                    
                 </Col>
                 <Col className="input-field">
                     <TextInput name="year" type="text" label="Year" />                    
