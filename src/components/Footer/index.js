da129908b74e8732729fa793dc4cdf7a4d246384
Phillip Chang <phillip-chang@live.com>
2019-08-04 15:11:48 -0700

    Debug navbar and add sign in icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,15 +8,13 @@ import './style.css';
 function Footer(){
     return (
 
-<div className="uk-container-fluid uk-column-1-2 uk-column-divider" id="footerContent">
+<div className="uk-container-fluid uk-column-1-1 uk-column-divider" id="footerContent">
 
 	<div className = "footerSection">
 		<div >
 	  	<Icon type="car" className = "icon uk-align-center"/>
 	  	 <p>© 2019 Copyright VENVI. All Rights Reserved.</p>
-	  </div>
-	  <h4>Chloe</h4>
-	 
+	  </div>	 
 	</div>
 
 	<div className = "footerSection">
