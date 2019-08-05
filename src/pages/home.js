9e51744acd7cb9d64f52266cb63e493215db8525
Chloe Zhou <chloezhouny@gmail.com>
2019-08-05 07:59:37 -0700

    Edit nav bar

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import ButtonBases from '../components/ButtonBases';
 import Container from '../components/Container';
 import Divider from '../components/Divider';
 import Footer from '../components/Footer';
+import Card from '../components/Card';
 import './index.css'
 
 
@@ -19,6 +20,7 @@ render () {
         	<h3> Top Consumer Picks </h3>
         	<span id="line"> </span>
         	<ButtonBases />
+            <Card />
         </Container>
         <Footer />
     </div>
