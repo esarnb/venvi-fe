26b58e83ca71032d23648718ae488238cf959656
Esar Behlum <esarnb@gmail.com>
2019-08-06 16:14:44 -0700

    change backend to esar's and remove proxy since we use axios.create

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: "https://cors-anywhere.herokuapp.com/https://venvi-be.herokuapp.com/",
-//   responseType: "json"
-// });
-
-
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  // baseURL: "https://cors-anywhere.herokuapp.com/https://venvi-be.herokuapp.com/",
+  baseURL: "https://cors-anywhere.herokuapp.com/https://venvi-passport2.herokuapp.com/",
   responseType: "json"
 });
+
+
+// export default axios.create({
+//   baseURL: "http://localhost:3001",
+//   responseType: "json"
+// });
