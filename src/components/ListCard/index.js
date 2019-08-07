4c0dc4b88450f1ef9d0753189a36f015181c1f42
Phillip Chang <phillip-chang@live.com>
2019-08-07 14:08:00 -0700

    Add listing card to user profile

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -14,7 +14,8 @@ import { yellow } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import DeleteSharpIcon from '@material-ui/icons/DeleteSharp';
+
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -72,9 +73,74 @@ export default function ListCard(props) {
             SLC
           </Avatar>
         }
+        title= {name}      
+        />
+      <CardMedia
+        className={classes.media}
+        image={image}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton id="favorite" aria-label="add to favorites">
+          <FavoriteIcon id="fav"/>
+        </IconButton>
+        {/* <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton> */}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Price: {price}</Typography>
+          <Typography paragraph>
+          </Typography>
+          <Typography paragraph>
+          </Typography>
+          <Typography paragraph>
+          </Typography>
+          <Typography>
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
+
+export function ListCardProfile(props) {
+  const {image,make,model,price,year} = props;
+  var name = `${year} ${make} ${model}`
+  console.log("name",name)
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
+
+  return (
+    <Card className={classes.card} id="list-card">
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            SLC
+          </Avatar>
+        }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
+          <IconButton aria-label="delete">
+            <DeleteSharpIcon />
           </IconButton>
         }
         title= {name}      
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import Container from '../components/Container';
 import UserProfile from '../components/UserProfile';
-import ListCard from '../components/ListCard';
+import { ListCardProfile } from '../components/ListCard';
+import { ListingAPI } from '../utils/API';
 
 //Component
 class Profile extends React.Component {
+
+state = {
+  userList: []
+}
+
+  componentDidMount(){
+  this.userListing();
+}
+
+userListing = () =>
+{
+  let userId = 2;
+    ListingAPI.getListingByUser(userId).then(res=>{
+      console.log("all listings databack");
+      console.log(res.data);
+      // console.log(data.data[0]);
+      this.setState({ userList:res.data });
+      console.log(this.state.userList)
+    });
+}
+
   render() {
     return (
       <div>
         <Container>
           <UserProfile />
-          <ListCard />
+          {this.state.userList.map(list =>(
+          <ListCardProfile key={list.id}
+            image={list.image}
+            make={list.make}
+            model={list.model}
+            price={list.price}
+            year={list.year}
+            vin={list.vin} />
+          ))}
         </Container>
       </div>
     );
