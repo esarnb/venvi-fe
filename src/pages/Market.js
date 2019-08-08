606db03dff4af1f2bc8073bf0979358d5a18f451
Chloe <chloezhouny@gmail.com>
2019-08-08 03:01:34 -0700

    Add footer for each page and fix padding, margin

--- src/pages/Market.js
+++ src/pages/Market.js
@@ -5,6 +5,9 @@ import ListForm from '../components/ListForm';
 import NavBar from '../components/NavBar';
 import { ListingAPI } from '../utils/API';
 import ListCard from '../components/ListCard';
+import Footer from '../components/Footer';
+import Container from '../components/Container';
+import Card from '../components/Card';
 import './index.css'
 
 
@@ -89,7 +92,7 @@ render () {
   })
   return (
     <div>
-    <NavBar/>
+      <div id = "wrapper">
         <h3 id="market-head"> Market </h3>
         <span id="market-line"></span>
         <div id="market-btn">
@@ -109,6 +112,9 @@ render () {
       vin={item.vin} />
   ))}
   </div>
+  </div>
+
+    <Footer />
     </div>
   );
 }
