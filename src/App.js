082d92c9fb2bea00995405e99e843a0b25267755
esarnb <esarnb@gmail.com>
2019-08-25 01:17:51 -0400

    checking state

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends React.Component {
     Test.getResponse();
     Test.putQuery(id);
     Test.deleteQuery(id);
+    console.log("\n\n\n\n\n CURRENT STATE: ", this.state, "\n\n\n\n\n")
   }
 
   // getUserByProfileId() {
