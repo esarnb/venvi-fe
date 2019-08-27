1164bf6d2c9ff81c3683cb0bec3dd546624c5e17
Chloe <chloezhouny@gmail.com>
2019-08-26 20:25:35 -0700

    Updates

---
+++ precache-manifest.f86d4a4a8068088a7fde4b7c8f138b63.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f659dbfd34d0b6a147af07f6f199131",
+    "url": "/venvi-fe/index.html"
+  },
+  {
+    "revision": "edd3b67a6545595e25c0",
+    "url": "/venvi-fe/static/css/2.472da827.chunk.css"
+  },
+  {
+    "revision": "f5846332179db190693e",
+    "url": "/venvi-fe/static/css/main.c537b7b1.chunk.css"
+  },
+  {
+    "revision": "edd3b67a6545595e25c0",
+    "url": "/venvi-fe/static/js/2.1623d9db.chunk.js"
+  },
+  {
+    "revision": "f5846332179db190693e",
+    "url": "/venvi-fe/static/js/main.414807a7.chunk.js"
+  },
+  {
+    "revision": "7b77bfe0db83383134dc",
+    "url": "/venvi-fe/static/js/runtime~main.6dba387e.js"
+  },
+  {
+    "revision": "4e45298129cc8367ff71917c8b5faa15",
+    "url": "/venvi-fe/static/media/diffuse.4e452981.jpeg"
+  },
+  {
+    "revision": "369bf98e513a2e7943bcae6b509eb619",
+    "url": "/venvi-fe/static/media/diffuse2.369bf98e.jpeg"
+  },
+  {
+    "revision": "cfbdca8166dc4c7b3f3b3d4a5c89a981",
+    "url": "/venvi-fe/static/media/diffuse3.cfbdca81.jpeg"
+  },
+  {
+    "revision": "2f67b49c34feef59c992be19138ca966",
+    "url": "/venvi-fe/static/media/down.2f67b49c.png"
+  },
+  {
+    "revision": "df9dfff0d11121e24054bedc9f20e78f",
+    "url": "/venvi-fe/static/media/spec.df9dfff0.jpeg"
+  },
+  {
+    "revision": "475682ea98bbed825b3fb932f359d748",
+    "url": "/venvi-fe/static/media/t.475682ea.obj"
+  },
+  {
+    "revision": "bf31b44e2dc23067e45a8ddf1be4b0ef",
+    "url": "/venvi-fe/static/media/t.bf31b44e.mtl"
+  }
+]);
